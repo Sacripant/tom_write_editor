7151e5ee02a4573d27e677de28666bf8d28e62a1
Sacripant <thomas@sacripant.fr>
2013-07-24 13:34:56 +0000

    copy textarea content in Ace Editor when click in full screen button intead only when page is load

--- ace-for-write-tab.js
+++ ace-for-write-tab.js
@@ -2,7 +2,7 @@
 	// Add button fullsceen		
 	var showAce	=	$('<a class="show-ace ace-show-hide"><i>fullscreen</i></a>').prependTo('p.body');
 	
-	// Initiation code html wrapper-fullscreen                                                      /
+	// Initiation code html wrapper-fullscreen
 	var fullscreen	=	$('                                                                   \
 							<div id="ace-fullscreen" class="show">                                \
 								<h1 class="ace-article_title"></h1>                                \
@@ -11,14 +11,14 @@
 									<button class="hide-ace ace-show-hide ace-fixed">close</button> \
 								</div>                                                             \
 							</div>                                                                \
-						').appendTo('body');
+							').appendTo('body');
 	
 	// Mode Textile par default
-	var changeMode = $('.ace-changeMode button');
-	$('.textileMode').addClass('actif');
-	changeMode.click(function() {
-		changeMode.toggleClass('actif');
-	});
+	// var changeMode = $('.ace-changeMode button');
+	// $('.textileMode').addClass('actif');
+	// changeMode.click(function() {
+	// 	changeMode.toggleClass('actif');
+	// });
 	
 	var aceEditor = document.getElementById('ace-editor');				
 	// aceEditor.style.fontSize='15px';
@@ -33,12 +33,14 @@
 		// THEME
 		editor.setTheme("ace/theme/sacripant");			
 		// synchronize with textarea
-		editor.getSession().setValue(body.val());
 		editor.getSession().on('change', function(){
 				body.val(editor.getSession().getValue());
 		});	
 		// show fullscreen
 		$('.ace-show-hide').click(function() {
+			// copy textarea content in Ace editor
+			editor.getSession().setValue(body.val());
+			// display editor in fullscreen
 			$('html').toggleClass('ace-editor-on');
 			// Clone titre article to Fullscreen
 			var titre = $('#title').val();
