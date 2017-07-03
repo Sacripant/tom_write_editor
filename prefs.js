687f5a4d262bff0c84a4e45d22d32e4e5e7bf3b4
Thomas Jund <thomas@sacripant.fr>
2017-07-03 11:32:22 +0200

    prefs: add articles link & set default URL for iframes

--- prefs.js
+++ prefs.js
@@ -1,12 +1,14 @@
 {
   "drop": {
+    "articles": "<txp:permlink id='{{ id }}'><txp:title /></txp:permlink>",
     "images": "<txp:image id='{{ id }}' />",
     "files": "<txp:file_download_link id='{{ id }}'> \r\n    <txp:file_download_name />\n</txp:file_download_link>",
     "links": "<txp:link id='{{ id }}'/>"
   },
   "path" : {
-  	"images" : "/sacripant/trunk/textpattern/?event=image",
-  	"files" : "/sacripant/trunk/textpattern/?event=file",
-  	"links" : "/sacripant/trunk/textpattern/?event=link"
+    "articles" : "?event=list",
+  	"images" : "?event=image",
+  	"files" : "?event=file",
+  	"links" : "?event=link"
   }
 }
