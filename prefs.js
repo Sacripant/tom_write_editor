63b0a26b9277ff6eaf29e6b610b959d5e0a2560d
Thomas Jund <thomas@sacripant.fr>
2017-07-03 16:07:48 +0200

    prefs: add right panel default size

--- prefs.js
+++ prefs.js
@@ -5,6 +5,7 @@
     "files": "<txp:file_download_link id='{{ id }}'> \r\n    <txp:file_download_name />\n</txp:file_download_link>",
     "links": "<txp:link id='{{ id }}'/>"
   },
+  "rightPanelDefaultSize" : "2",
   "path" : {
     "articles" : "?event=list",
   	"images" : "?event=image",
