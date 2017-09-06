e1108ffc7e8335954924283d621387b4cfb03111
Thomas Jund <thomas@sacripant.fr>
2017-09-06 17:42:12 +0200

    delet addiitonal snippets and prefs-user

--- additional-snippets.js
+++ additional-snippets.js
@@ -1,12 +1,1 @@
-[
-	{
-		"name" : "body_img",
-		"tabTrigger" : "bodyimg",
-		"content" : "<txp:body_img img_id='${1:id}' grid='${2:span1-2}' ref='${3:a}' border='${4:0}' />"
-	},
-	{
-		"name" : "body_wrapper",
-		"tabTrigger" : "bodygrid",
-		"content" : "<txp:body_wrapper grid='${1:grid}' to-aside='${2:to-aside}' />"
-	}
-]
+[]
